89c60b8af73c8fea7092876ec7e21edbd613d71b
JeffreyBerger <jeffrey.j.berger@gmail.com>
2013-09-30 23:49:41 -0400

    html1 lesson

--- html1.php
+++ html1.php
@@ -5,14 +5,14 @@
 
       <div class="jumbotron">
         <h1>Basic HTML and CSS</h1>
-        <h3>We'll make some basic webpage with HTML, CSS, and Bootstrap</h3>
         
-        <p>I'm not going to go through a whole lot of HTML and CSS here just a description of what they are and some practices with them.  I'll direct you to the W3 schools <a href="http://www.w3schools.com/html/default.asp" target="blank">html</a> and <a href="http://www.w3schools.com/css/default.asp">CSS</a> tutorials for a more complete coverage.  I also won't be describing a follow-my-lead website with HTML and CSS in this lesson, you've already probably used some in some other lesson and you'll be using it constantly through other lessons so that will be enough practice for you.</p>
+        <p>I'm not going to go through a whole lot of HTML and CSS here just a description of what they are and some practices with them.  I'll direct you to the W3 schools <a href="http://www.w3schools.com/html/default.asp" target="blank">html</a> and <a href="http://www.w3schools.com/css/default.asp">CSS</a> tutorials for a more complete coverage.  I also won't be building this tutorial like the others, a follow-my-lead website with HTML and CSS, you've already probably used some in some other lesson and you'll be using it constantly through other lessons so that will be enough practice for you.</p>
 
-        <p>HTML is not a programming language, it is a markup language - this means that when you mess up your syntax things simply don't look the way that you would expect, not that the entire page crashes down.  HTML consists of sets of tags which are opened and closed to deliminate your content.  A paragraph is deliminated by a 'p' tag and it looks like this</p>
+        <p>HTML is not a programming language, it is a markup language - this means that when you mess up your syntax things simply don't look the way that you would expect, it does not crash the entire page.  HTML consists of sets of tags which are opened and closed to deliminate your content.  A paragraph is deliminated by a 'p' tag and it looks like this</p>
         <code>
         	&ltp&gtYour Paragraph Here&lt/p&gt
         </code>
+
         <p>The block that I put the code in to make it stand out is actually a &ltcode&gt block.  There are some tags which are known as 'inline' elements and some are 'block' elements.  Inline elements do not have a new-line automatically associated with them while block elements do.  The &ltp&gt tag is a block element while the &ltspan&gt tag is an inline element (span is like a p tag just inline).  You use HTML only to give structure to your page, all styline comes from CSS, which we'll discuss more a bit later.</p>
 
         <p>There are a lot of different types of HTML tags as well as special things such as forms, but I won't disucss them really.  I want to mention how you can give HTML elements properties thought.  There are some special properties for HTML elements, such as id and class.  These are identifiers that allow the Javascript or CSS to identify which HTML element it is refering to.  The id tag must be unique or you'll run into issues when trying to reference it (and since HTML is not a programming language you don't have a compiler to give you warnings of duplicates!).  The class is not unique and may be assigned to many different elements, not even of the same type.  You can also assign other properties such as name, or my_value.  The way you assign these things to an HTML tag is by placing them inside the tag in the following way</p>
@@ -21,7 +21,7 @@
         	&ltp id="this_p" class="type_A" name="this_p_name" my_value="42"&gtHola&lt/p&gt
         </code>
 
-        <p>And if you are wondering how I put that code in there, things that are in code brackets are still evaluated by the browser as HTML so instead of actually using the bracket &lt you should use the code $amplt.  Same for the right bracket but with &ampgt, and to type those out you should use &ampamp so &amplt is actually coded as &ampamplt (and &ampamp is written &ampampamp... I know it is a bit much...).</p>
+        <p>And if you are wondering how I put that code in there, things that are in code brackets are still evaluated by the browser as HTML so instead of actually using the bracket &lt you should use the code &amplt.  Same for the right bracket but with &ampgt, and to type those out you should use &ampamp so &amplt is actually coded as &ampamplt (and &ampamp is written &ampampamp... I know it is a bit much...).</p>
 
         <p>CSS is a "cascading style sheet", which is again not a programming language.  CSS is what styles the document in all respects.  This includes colors, fonts, sizes, and positioning of all elements.  If you just write a standard HTML file with no CSS you will have a list of text and pictures because everything will be in a single column left aligned.  You can try it out, and it is instructive to see what HTML without CSS is, it is only the barebones structure of the document.  CSS does not have that many rules but getting what you want out of CSS and having all the rules play well with each other is a different matter and is non-trivial.  I hate CSS.  There are three different places where you can place the CSS to an HTML file.  The first place is in a separate file and to refrence it at the beginning of your HTML file.</p>
 
