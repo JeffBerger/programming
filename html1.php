e687b8fb5f2c89a9935108bb7033bd6a5da783ce
JeffreyBerger <jeffrey.j.berger@gmail.com>
2013-09-30 23:36:03 -0400

    html1 lesson

--- html1.php
+++ html1.php
@@ -21,11 +21,44 @@
         	&ltp id="this_p" class="type_A" name="this_p_name" my_value="42"&gtHola&lt/p&gt
         </code>
 
-        <p>And if you are wondering how I put that code in there, things that are in code brackets are still evaluated by the browser as HTML so instead of actually using the bracket &lt you should use the code \&lt.  Same for the right bracket but with gt.</p>
+        <p>And if you are wondering how I put that code in there, things that are in code brackets are still evaluated by the browser as HTML so instead of actually using the bracket &lt you should use the code $amplt.  Same for the right bracket but with &ampgt, and to type those out you should use &ampamp so &amplt is actually coded as &ampamplt (and &ampamp is written &ampampamp... I know it is a bit much...).</p>
 
-        <p>CSS is a "cascading style sheet", which is again not a programming language.</p>
+        <p>CSS is a "cascading style sheet", which is again not a programming language.  CSS is what styles the document in all respects.  This includes colors, fonts, sizes, and positioning of all elements.  If you just write a standard HTML file with no CSS you will have a list of text and pictures because everything will be in a single column left aligned.  You can try it out, and it is instructive to see what HTML without CSS is, it is only the barebones structure of the document.  CSS does not have that many rules but getting what you want out of CSS and having all the rules play well with each other is a different matter and is non-trivial.  I hate CSS.  There are three different places where you can place the CSS to an HTML file.  The first place is in a separate file and to refrence it at the beginning of your HTML file.</p>
 
-        <p>Bootstrap</p>
+        <code>
+        	&ltlink rel="stylesheet" href="myfile.css" type="text/css" media="screen" /&gt
+        </code>
+
+        <p>This is the best way to do it, now all your CSS is in one place and you know where to modify it.  This is also the most easily overridden.  The second method overrides this and that is to put your CSS directly in the HTML but at the beginning.</p>
+
+        <code>
+        	<p>&lthead&gt</p>
+        	<p>&ltstyle&gt</p>
+        	<p>&ltstyle type="text/css"&gt</p>
+			<p>body { </p>
+			<p?	background-color: #fff;</p>
+			<p>	margin: 40px;</p>
+			<p>}</p>
+        	<p>&lt/head&gt</p>
+        	<p>&lt/style&gt</p>
+        </code>
+
+        <p>This is better as all the CSS is in one place but not as good because if you want to use the same CSS for multiple HTML files, as one is wont to do on a website where you are having a uniform style, you would need to copy and paste your header into every file.  Then when you want to change, say, your background color you need to go into each file and change it there.  Also if you have a CSS file you are including, this block at the top will override it, so combining both of them could be obnoxious because if you changed your background color in the file it might not change in the page because it was overridden in this style block.  The third way of including CSS is inline, which is the worst for everyone.</p>
+
+        <code>
+        	&ltp style="margin:10px;color:blue;"&gtText&lt/p&gt
+        </code>
+
+        <p>This overrides all the other methods of putting CSS in, it is the least universal (it applies to only that one element), and it is the hardest to track down and change, especially as it overrides all the other methods mentioned.  Don't do this.  I'm not going to discuss CSS more because I'm not qualified to.  I'm bad at it, and that makes me want to use it less reinforcing my shittiness.  You guys do it.</p>
+
+        <p>Bootstrap is a package by twitter (hence it is called <a href="http://getbootstrap.com/">twitter bootstrap</a>) that was built to help websites attain a uniform and professional look quickly and without pain, and for me it is my CSS savior.  Bootstrap takes care of most of the CSS for you and leaves only the little stylistic tweeks for you to manage - glory.  This site is using bootstrap.  In its essence bootstrap has two components, javascript and CSS.  We'll discuss just the CSS now.  All you need to do is include the twitter bootstrap css file in the head of your HTML document (You can use a CDN to do this, which is a content-delivery-network, a method of hosting files with low latency.  There are ones which allow you to upload your own files and others which have files hosted that you can use, we'll use CDNJS which is public).</p>
+
+        <code>
+        	&ltlink href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.0.0-rc1/css/bootstrap.min.css" rel="stylesheet"/&gt
+        </code>
+
+        <p>Now you can begin using custom classes such as "container" which creates a centered contained, "jumbotron" which creates a centered area that is shaded a different color for text, and the glorious scalable columns that the grid framework has on bootstrap.  Literally all there is to bootstrap is including the CSS and then using a bunch of premade CSS classes which, for the most part, do the annoying work for you (making columns that are proportional and scale with the browser window, getting nice looking buttons, in conjunction with <a href="http://fortawesome.github.io/Font-Awesome/">font awesome</a> a lot of icons for you, and did I mention rescalable columns?).  We'll talk more about the javascript in another lesson, for now just play around with the classes that you find on the bootstrap page in both the <a href="http://getbootstrap.com/css/">css</a> and the <a href="http://getbootstrap.com/components/">components</a> section.  They have example code for all of it so you can copy, paste, and then modify.  It is phenominal.</a>
+
       </div>
 
     </div> <!-- /container -->
