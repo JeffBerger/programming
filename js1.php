8f0f842df908bbc3de87500d798fd7e962ee76ab
JeffreyBerger <jeffrey.j.berger@gmail.com>
2013-10-05 13:23:55 -0400

    draft2

--- js1.php
+++ js1.php
@@ -9,7 +9,7 @@
 
         <h3>Pre-reqs : HTML/CSS</h3>
 
-        <h4>Intro</h4>
+        <h2>Intro</h2>
 
         <p>Many approaches to javascript tie it heavily to what it does with HTML elements in a webpage.  I'll discuss this a bit but I'd like to more discuss how to use javascript in a general programming manner, as it is a complete programming language.  For me I used it for little doodads in a webpage before I realized how empowered it is, so I'd like you to start off knowing you can program basically anything in just javascript.</p>
 
@@ -31,7 +31,7 @@
 		<p>Now go to the file and open it in chrome.  First, your life will be a lot easier if you just submit and use chrome.  Second, I want you to consider for a moment that we aren't doing any of this on a webserver.  You don't need a webserver for any of this javascript stuff, javascript is executed 'client side', which means your end user executes the javascript code in their browser.  Your web server will send them the javascript code - all of the javascript code - and they will run it on their machine.  So you are just skipping the step where the webserver sends you the file and you are just keeping it on your machine, totally legitimate.  The web browser is the program that will interpret the javascript and run it as a program, also super easy.  It is really nice, and you don't have to deal with logging into a webserver and configuring it so it can accept requests or compile things.  You should remember always that when you make javascript you send the whole code to your end user, which means they can do anything they want with it - dice it up, steal data inside it, or steal the whole code.  Never put sensitive data in your javascript that you wouldn't want the user to get access to because as you'll see in later tutorials a user can get to ANYTHING you give them.  Never trust the user.</p>
         <p>When you open the file in chrome you'll see a window pop up that will say "hello" and won't let you do shit till you dismiss it.  This is an alert and is the most annoying and easy function call to make.  A function call is a way of executing some other piece of code which will perform a task.  We'll make our own functions a bit later.  Lets talk about variables.</p>
 
-        <h4>Variables, HTML, and JQuery</h4>
+        <h2>Variables, HTML, and JQuery</h2>
 
         <p>Javascript is not "strongly typed", which means when you declare a variable then you don't need to tell it if it is a string, an integer, or a decimal number (called floating point numbers).  All variables are declared like this : </p>
         <code>
@@ -72,21 +72,23 @@
 		<p>The first block of script is to import a package called jquery.  Jquery is the most widely used package in javascript and it has a lot of functions to select HTML elements in an easy manner.  It is cross browser as well, one of the biggest issues with javascript is that the main browsers (especially IE) all implement javascript slightly different, which means what works in chrome might not work in firefox.  JQuery works in all of the browsers, so lets use it.  In JQuery the $ sign is shorthand for the JQuery library so we'll be calling a lot of functions from JQuery.  The most useful is the JQuery selector which is $("#element_id") to select a single element with an id, if you want to select all elements of a class you can use $(".class_name").  There are others but you can look those up, I'm here to give you a start not to be comprehensive.  Lets change our script (which is after the body but before the closing of the html tag) to put the HTML at the beginning of the output div (we will prepend it) instead of using alerts.</p>
 
         <code>
-        	<p>var i = 10;</p>
-        	<p>var j = "josh";</p>
-        	<p>var k = "kat";</p>
-        	<p>$("#output").prepend("&ltp&gt" + i + "&lt/p&gt");</p>
-        	<p>$("#output").prepend(""&ltp&gt"The variable j is " + j + "&lt/p&gt");</p>
-        	<p>$("#output").prepend(""&ltp&gt"The variable k is " + k + "&lt/p&gt");</p>
-        	<p>j = j + k;</p>
-        	<p>i = i + 1;</p>
-        	<p>$("#output").prepend("&ltp&gt" + j + "&ltp&gt");</p>
-        	<p>$("#output").prepend("&ltp&gt" + i + "&ltp&gt");</p>
+            <p>&ltscript&gt</p>
+        	<p>&nbsp;&nbsp;&nbsp;var i = 10;</p>
+        	<p>&nbsp;&nbsp;&nbsp;var j = "josh";</p>
+        	<p>&nbsp;&nbsp;&nbsp;var k = "kat";</p>
+        	<p>&nbsp;&nbsp;&nbsp;$("#output").prepend("&ltp&gt" + i + "&lt/p&gt");</p>
+        	<p>&nbsp;&nbsp;&nbsp;$("#output").prepend("&ltp&gtThe variable j is " + j + "&lt/p&gt");</p>
+        	<p>&nbsp;&nbsp;&nbsp;$("#output").prepend("&ltp&gtThe variable k is " + k + "&lt/p&gt");</p>
+        	<p>&nbsp;&nbsp;&nbsp;j = j + k;</p>
+        	<p>&nbsp;&nbsp;&nbsp;i = i + 1;</p>
+        	<p>&nbsp;&nbsp;&nbsp;$("#output").prepend("&ltp&gt" + j + "&ltp&gt");</p>
+        	<p>&nbsp;&nbsp;&nbsp;$("#output").prepend("&ltp&gt" + i + "&ltp&gt");</p>
+            <p>&ltscript&gt</p>
         </code>
 
         <p>Notice how I now am putting HTML tags in what is being prepended to the output div.  This is because we are putting it into the HMTL div so we need to have HTML formatting.  Without the p tags the browser would simply paste all the text into one long string (go ahead and try it and you'll see what I mean).</p>
 
-        <h4>The Godlike For Loop</h4>
+        <h2>The Godlike For Loop</h2>
 
         <p>The command "i = i + 1;" is called an incrememnt and it is very common to do this to keep counts so there is a shorthand for it, "i++;".  There is also a decrement and I will let you guess what that is, it goes by "i--;".  This is especially useful if you want to do something multiple times, which is most easily accomplished inside a loop.  There are several different loops, the most common ones are the "while" loop, the "do while" loop, and the lord of all - the "for" loop.  Let us do a for loop right now. A for loop will repeat the code inside of the loop and it takes three parameters.  First, an initializer which it sets when the loop starts.  Second, a test, the loop will continue until that test is no longer true.  Third, an incrementer, which is a command that it will execute every time the loop executes a single iteration.  Let's see one in practice.  Replace what is inside the script tags at the end of your program with the following code:</p>
 
@@ -98,7 +100,7 @@
 
         <p>Super easy, now you'll see it will add ten lines of code each of them slightly different.  First we are using the append instead of prepend, this just means new lines are put at the top of the HTML and not at the end of it.  What is happening in the for loop is the loop is creating a variable "i" and setting it to 0.  This is our counter for the for loop, and it will tell us what iteration we are on.  If you imagine that we havea  list of things we want to go through that is 100 elements we don't want to manually go through each, so we make a for loop that will go through 100 things and execute some code on each element.  The way the for loop knows when to stop is the second argument, which says that the for loop should keep going as long as i is less than 10.  As long as i is less than 10 that statement is "true" and the loop will keep going.  Now if we don't make i go up with each iteration we could have a loop that goes on forever and that is a very possible scenario.  So the third argument is a command that is execute each time the loop iterates once, whichi in this case we simply increase i by one.  This means that it will go through the loop 10 times (0-9 is ten times, if you want 1-10 then just start i at 1 and do i&lt11).  For loops are god.  Let us do a quick detour and discuss an array and how to use it with a for loop that is specialized for them.</p>
 
-        <h4>Arrays</h4>
+        <h2>Arrays</h2>
 
         <p>Lets talk about arrays for a minute.  We can have a variable map to multiple items by putting them in a list called an array.  We can access them by defining the number of the entry we want.</p>
 
@@ -118,18 +120,71 @@
             <p>}</p>
         </code>
 
-        <p>Ok works great.  Not done yet, there is a special for loop that we can use with an array where we don't even have to specify an iterator or anything, it is a for loop that uses the in operator.  This will just pull the elements from the array with each loop and allow us to play with them, in this case we'll just print them out.</p>
+        <p>Lets modify something in the array now.  This is easy as selecting the element by number and replacing it with an equals operation.</p>
 
         <code>
             <p>var tools = ["hammer","anvil","penis"];</p>
+            <p>tools[2] = "Another Hammer";</p>
+            <p>for(var i = 0;i&lt3;i++){</p>
+            <p>&nbsp;&nbsp;&nbsp;$("#output").append("&ltp&gtTool " + i + " " + tools[i] + "&lt/p&gt");</p>
+            <p>}</p>
+        </code>        
+
+        <p>Fine, but what if we want to add something on?  We can do that by specifying a numeric selector that is beyond the scope of our array.  Lets do that.</p>
+
+        <code>
+            <p>var tools = ["hammer","anvil","penis"];</p>
+            <p>tools[3] = "Another Hammer";</p>
+            <p>for(var i = 0;i&lt4;i++){</p>
+            <p>&nbsp;&nbsp;&nbsp;$("#output").append("&ltp&gtTool " + i + " " + tools[i] + "&lt/p&gt");</p>
+            <p>}</p>
+        </code>         
+
+        <p>Notice how we had to extend the reach of our loop as well to keep track of how long the array is.  Less than helpful.  We can automatically get the length of the array by using the "MY_ARRAY.length" property.  We'll replace our for loop with that and see how it works.</p>
+
+        <code>
+            <p>var tools = ["hammer","anvil","penis"];</p>
+            <p>tools[3] = "Another Hammer";</p>
+            <p>for(var i = 0;i&lttools.length;i++){</p>
+            <p>&nbsp;&nbsp;&nbsp;$("#output").append("&ltp&gtTool " + i + " " + tools[i] + "&lt/p&gt");</p>
+            <p>}</p>
+        </code>         
+
+        <p>Now the program figures out how long the array is itself when it gets to the loop, great.  But there is one little catch, we can assign things to memory slots that are not contiguous and we'll be in for a problem.  Watch what I mean.</p>
+
+        <code>
+            <p>var tools = ["hammer","anvil","penis"];</p>
+            <p>tools[7] = "Another Hammer";</p>
+            <p>for(var i = 0;i&lttools.length;i++){</p>
+            <p>&nbsp;&nbsp;&nbsp;$("#output").append("&ltp&gtTool " + i + " " + tools[i] + "&lt/p&gt");</p>
+            <p>}</p>
+        </code>        
+
+        <p>Length is still four, but now we would need to do "tools[7]" to find where we hid the other hammer.  This causes us an issue, luckily there is a special way of calling a for loop which will allow us not to have to worry about that either, it is a for loop that uses the in operator.  This will just pull the elements from the array with each loop and allow us to play with them, in this case we'll just print them out.</p>
+
+        <code>
+            <p>var tools = ["hammer","anvil","penis"];</p>
+            <p>tools[7] = "another Hammer";</p>
             <p>for(element in tools){</p>
-            <p>&nbsp;&nbsp;&nbsp;$("#output").append("&ltp&gtTool " + element + "&lt/p&gt");</p>
+            <p>&nbsp;&nbsp;&nbsp;$("#output").append("&ltp&gtTool " + element + " " + tools[element] + "&lt/p&gt");</p>
             <p>}</p>
         </code>
 
-        <p>Super easy for an array and very conveinent.  Notice that "element" has not been declared before, this variable is created and to it is assigned each subsequent value in the array until we are out of elements in the array.</p>
+        <p>Super easy for an array and very conveinent.  Notice that "element" has not been declared before, this variable is created and to it is assigned each subsequent integer valuein the array until we are out of elements in the array.  So element actually gets the values "element 0, 1 , 2 ..." and we can pull the value by using the tools[element] selector to get the element.  Now we do not have to know the position of the elements or their length to iterate through them.  You'll see this usage later in objects in the exact same context.  One last thing, what if we want to add somehting contiguously onto the end of an array but we don't want to have to remember where the array end is.  That is easy, we don't need to specify a memory location just use a blank brace and jvaascript will find the end for you.  You can still get some unique behaviors though if you are not careful.  Comtemplate this example.</p>
 
-        <h4>If statements</h4>
+        <code>
+            <p>var tools = ["hammer","anvil","penis"];</p>
+            <p>tools[] = "Sam's hammer";</p>
+            <p>tools[7] = "another Hammer";</p>
+            <p>tools[] = "Hail to the Hammer!";</p>
+            <p>for(element in tools){</p>
+            <p>&nbsp;&nbsp;&nbsp;$("#output").append("&ltp&gtTool " + element + " " + tools[element] + "&lt/p&gt");</p>
+            <p>}</p>
+        </code>
+
+        <p>Notice that the first blank brace adds "Sam's Hammer" to the end of the array, which ends at memory location 2, so it goes to location 3.  Now when we use the second blank brace for "Hail to the Hammer!" we get that we are in memory location 8, because the last element in the array is now in location 7.  The blank brace does not find the next empty memory slot it finds the end of the array and tacks this element onto it.</p>
+
+        <h2>If statements</h2>
 
         <p>Now let's rewind to before when we were just printing the loop number and get a bit more complicated and just output a line if it is an even number.  For this we need to use an if statement.  If statements are logical gates which take a statement that they will evaluate and see if the statement is true.  If it is true then it will execute a block of code, if it isn't then it will skip it.  Let us do an example using the modulus operator "%".  This gives you the remainder of division, so 9%4 = 1 because if I divide 9 by 4 I get 2 with a remainder of 1, so the modulus operator returns this remainder.  If we are interested in evens or odds then we want to do modulus of 2.  8 % 2 = 0 because there is no remainder but 9 % 2 = 1 because there is a remainder of 1.  So even numbers will have 0 when modulus of two is evaluated and odd numbers will be 1.  Lets put this into our for loop.</p>
 
@@ -207,7 +262,7 @@
 
         <p>We are using the inline comment and the block comment.  The // comments everything at that point and past in that line (you can have comments at the end of a line to give an explanation to that line of code) and /* comments everything from that point onwards until it reaches */.  Lets talk about functions before we go further.</p>
 
-        <h4>Functions</h4>
+        <h2>Functions</h2>
 
         <p>A function is simply a block of code that you separate out from other parts of your code that does a specific purpose.  This makes the code more readable and you can use the function in different places, so if you are doing the same thing in multiple places you don't have to repeat typing the same lines of code, just call the function.  The function does not know about any variables that have been declared outside of it, so you need to give it information by passing it variables (this is known as scope).  Functions are very important and it is important that you are comfortable using them.</p>
 
@@ -303,7 +358,7 @@
 
         <p>Note how we construct the function in place, this is how we can create an anonymous function for specific events (which we'll see later) and AJAX calls (Javascript lesson 2).  We can declare a function for it and reference it or we can use an anonymous function, both are valid but you should be able to do both and comfortable with seeing either.</p>
 
-        <h4>Input and Events</h4>
+        <h2>Input and Events</h2>
 
         <p>Ok enough of the general programming let's get some user input on this bitch.</p>        
 
@@ -367,7 +422,7 @@
 
         <p>Notice that we put this "evt" as the only argument for the function.  This can be called whatever you want, "e", "event", "bushwick", whatever.  The name you put in there becomes the 'event object' that you are assigned from the event firing.  This object can give you information about what fired the event and other properties of the event.  Here we don't need it because the fact that we are triggering off of the id of the HTML element and the id uniquely identifies the element, but we could trigger instead for any click on a class - which could have multiple HTML elements and in that case we might want to use the event object to narrrow things down more (as an example).  You can do more reading at JQuery on the event object if you want.  There are also lot of events to listen to, and a lot of things you can do - and too many subtlties that I don't know to teach you.  This is also something you'll have to look up.  I've given you a start and you can google the rest.  But before we finish up I'm talking about an event object, but we haven't discussed objects in Javascript yet.  Let's do that.</p>
 
-        <h4>Objects</h4>
+        <h2>Objects</h2>
 
         <p>Objects in javascript are nothing but key-value pairs.  That is it.  You don't have to worry about anything more complicated than that.  You can access values in an object with the 'dot' notation.  Let's make an object and access the data in it.</p>
 
